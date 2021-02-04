ae4c73ce6d6eb111cf8b5451b21e166f75ed7e31
avilnet <avilnetpr@gmail.com>
2021-02-04 02:17:19 +0000

    task 2 adv

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - putchar text
- * Return - 1 
+* main - no putchar text
+* Return: 1
 */
 int main(void)
 {
-	write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
