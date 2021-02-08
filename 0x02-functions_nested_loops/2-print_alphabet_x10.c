bb57ffed8d8e35261f8fab2786beaace69aa9688
avilnet <avilnetpr@gmail.com>
2021-02-08 20:50:09 +0000

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - print 10 times the alphabet.
+ * Return: 0
+ **/
+void print_alphabet_x10(void)
+{
+	int i, a;
+
+	for (i = 0; i <= 9; i++)
+
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+}
+}
