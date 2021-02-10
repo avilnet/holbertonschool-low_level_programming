d64b02365bd0707e05ca525d0f163eb716214362
avilnet <avilnetpr@gmail.com>
2021-02-10 02:33:23 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - adds 2 integers and returns results
+ * @n: first number to add
+ * @m: second ## to add
+ * Return: results
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
