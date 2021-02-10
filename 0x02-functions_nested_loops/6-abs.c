70f10be7080d15ce175e6a964f835df73f13a436
avilnet <avilnetpr@gmail.com>
2021-02-10 01:02:01 +0000

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _abs - absolute number
+ * @x: integer
+ * Return: x
+ */
+int _abs(int x)
+{
+	if (x > 0)
+	{
+		x = x * 1;
+	}
+	else if (x < 0)
+	{
+		x = x * -1;
+	}
+	return (x);
+}
