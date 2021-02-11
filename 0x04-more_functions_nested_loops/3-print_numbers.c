1168d1ab748203640e2afe859e2795c3085aebfb
avilnet <avilnetpr@gmail.com>
2021-02-11 19:45:57 +0000

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
- * print numbers
+ * print_numbers - print the numbers
  * Return: 0
  */
 void print_numbers(void)
 {
 	int i = 0;
-	
+
 	for (i = '0'; i <= '9'; i++)
 		_putchar(i);
 	_putchar('\n');
