a800b3369bd927f8aec44b798bba46995fb246e0
avilnet <avilnetpr@gmail.com>
2021-02-11 18:15:40 +0000

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_diagonal - diagonal line
+ * @n: int
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int c = 0;
+	int s;
+
+	if (c <= 0)
+		_putchar('\n');
+	for (; c <= n - 1; c++)
+	{
+		for (s = 0; s < c ; s++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
